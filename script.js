e7d932ec805634675f06079a712219dad9a77495
SumanSaha <8759127658itisaha@gmail.com>
2024-01-18 23:40:03 +0530

    change3

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 let searchInput = document.getElementById('searchinput');
 let searchBtn = document.getElementById('searchBtn');
 let imageresult = document.querySelector('.imageresult');
+let searchForm = document.querySelector('#searchform')
 let showMoreBtn = document.getElementById('showmorebtn');
 let page = 1;
 let accesskey = "H__bTtMw6_kfpvA9HZAI9K0PHjSizELPiP7p8LYcoGM";
 
 
-async function getImages(){
-let url = "https://api.unsplash.com/search/collections?page="+page+"&query="+"cats"+"&client_id="+accesskey;
-let response = await fetch(url);
-let data = await response.json();
-console.log(data)
+async function getImages() {
+    let url = "https://api.unsplash.com/search/collections?page=" + page + "&query=" + searchInput.value + "&client_id=" + accesskey;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data)
 }
 
-getImages()
+searchForm.addEventListener('keydown', (e) => {
+    e.preventDefault();
+    page = 1;
+    if (e.key === 'Enter') {
+        getImages()
+    }
+})
