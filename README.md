90379bf9cc02890133d6e4afa1e0001d3b9cceb3
SumanSaha <8759127658itisaha@gmail.com>
2024-01-19 18:16:25 +0530

    final_changes

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# ImageSearchEngine
+Image Search Engine
+Overview
+This is a simple image search engine that utilizes the Unsplash API to fetch and display images based on user queries. Users can enter search terms, and the application will retrieve relevant images from Unsplash.
+
+Features
+Search for images by entering keywords.
+Display images in a responsive grid layout.
+"Show More" button to load additional images.
+Technologies Used
+HTML
+CSS
+JavaScript
+External APIs
+Unsplash API - Used for fetching high-quality images.
+Usage
+Open index.html in your web browser.
+Enter a search query in the input field.
+Click the "Search" button to display relevant images.
+Optionally, click "Show More" to load more images.
+Configuration
+To use the Unsplash API, you'll need to obtain an access key. Follow the Unsplash API documentation to create an account and get your access key. Replace the accesskey variable in script.js with your key.
+
+javascript
+Copy code
+let accesskey = "YOUR_UNSPLASH_ACCESS_KEY";
+Credits
+This project was created by Suman Saha as a personal project. Special thanks to Unsplash for providing a vast collection of high-quality images.
+
+License
+This project is licensed under the MIT License.
--- script.js
+++ script.js
@@ -8,24 +8,33 @@ let accesskey = "H__bTtMw6_kfpvA9HZAI9K0PHjSizELPiP7p8LYcoGM";
 
 
 async function getImages() {
-    let url = "https://api.unsplash.com/search/collections?page=" + page + "&query=" + searchInput.value + "&client_id=" + accesskey;
+    let url = "https://api.unsplash.com/search/collections?page=" + page + "&query=" + searchInput.value + "&client_id=" + accesskey + "&per_page=12";
     let response = await fetch(url);
     let data = await response.json();
-    console.log(data)
+    if (page === 1) {
+        imageresult.innerHTML = "";
+    }
     let results = data.results;
-    results.map((result)=>{
-        let images = document.createElement('img');
-        images.src = result.urls.small;
-        let imageLink = document.createElement('a');
-        imageLink.href = results.link.html;
+    results.map((result) => {
+        console.log(result)
+        const image = document.createElement('img');
+        image.src = result.cover_photo.urls.small;
+        const imageLink = document.createElement('a');
+        imageLink.href = result.cover_photo.links.html;
         imageLink.target = '_blank';
-        imageLink.appendChild(images);
+        imageLink.appendChild(image);
         imageresult.appendChild(imageLink)
+        showMoreBtn.style.display = "block"
     })
 }
 
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
     page = 1;
+    getImages()
+})
+
+showMoreBtn.addEventListener('click', () => {
+    page++;
     getImages()
 })
